501f3488f7611d40e70dceecbfe1fae817640fef
lewisvalaitis <valaitislewis@gmail.com>
2022-08-06 21:45:45 +0100

    Initial Commit

---
+++ Paint The Town/Paint_The_TownApp.swift
@@ -0,0 +1,17 @@
+//
+//  Paint_The_TownApp.swift
+//  Paint The Town
+//
+//  Created by Lewis Valaitis on 06/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Paint_The_TownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
